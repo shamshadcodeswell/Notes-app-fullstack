56446ed4596c6603688dde4ae0283abe13697d0a
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-07-10 17:49:09 +0530

    did the database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { connectDB } from "./db/connection.js";
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log(`app started listening to the port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Error in starting the server", error.message);
+    } else {
+      console.log("Error in starting the server", error);
+    }
+  }
+})();
