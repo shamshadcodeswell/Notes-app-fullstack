a8e35689690d9903257170b12356396bdb994eb0
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-07-16 23:42:39 -0700

    added search, sort functionality

--- client/src/components/NoteList.tsx
+++ client/src/components/NoteList.tsx
@@ -5,7 +5,9 @@ import "./NoteList.css";
 
 const NoteList = () => {
   const [list, setList] = useState<Note[]>([]);
-  const [isEmpty, setIsEmpty] = useState<boolean>(false);
+  const [sortOpen, setSortOpen] = useState<boolean>(false);
+  const [filteredList, setFilteredList] = useState<Note[]>([]);
+  const [search, setSearch] = useState<string>("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,46 +17,114 @@ const NoteList = () => {
       console.log(json);
       if (json.data) {
         setList(json.data);
-        setIsEmpty(false);
-      } else {
-        setIsEmpty(true);
-        console.log(isEmpty);
+        setFilteredList(json.data);
       }
     }
     fetchNotes();
   }, []);
 
+  const sortByNewest = () => {
+    setFilteredList(
+      [...filteredList].sort(
+        (a, b) =>
+          new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime(),
+      ),
+    );
+  };
+  const sortByOldest = () => {
+    setFilteredList(
+      [...filteredList].sort(
+        (a, b) =>
+          new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime(),
+      ),
+    );
+  };
+
+  const searchList = () => {
+    setFilteredList(
+      [...list].filter((note) =>
+        note.title.toLowerCase().includes(search.toLowerCase()),
+      ),
+    );
+  };
+
   return (
-    <div className="noteListContainer">
-      <div className="createTaskButtonContainer">
-        <button
-          className="createTaskButton"
-          onClick={() => navigate("/create")}
-        >
-          Create a note
+    <div>
+      <div className="searchBarContainer">
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Search Title"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+        <button className="SearchSumbitButton" onClick={() => searchList()}>
+          Search
         </button>
       </div>
-
-      {isEmpty ? (
-        <h1 className="emptyMessage">Your notes are empty</h1>
-      ) : (
-        list.map((note) => (
-          <div
-            key={note._id}
-            className="note"
-            onClick={() => navigate(`/note/${note._id}`)}
-          >
-            <h2 className="noteTitle">{note.title}</h2>
-            <h3 className="createdAt">
-              Created : {new Date(note.createdAt).toLocaleDateString("en-IN")}
-            </h3>
-            <h3 className="lastModified">
-              Updated : {new Date(note.updatedAt).toLocaleDateString("en-IN")}
-            </h3>
-            <p className="bodySnippet">{note.body}</p>
+      <div className="noteListContainer">
+        <div className="topbar">
+          <div className="createTaskButtonContainer">
+            <button
+              className="createTaskButton"
+              onClick={() => navigate("/create")}
+            >
+              Create a note
+            </button>
           </div>
-        ))
-      )}
+          <div className="sortingContainer">
+            <button
+              className="sortButton"
+              onClick={() => setSortOpen(!sortOpen)}
+            >
+              Sort
+            </button>
+            <div className={`sortingOptions ${sortOpen ? "show" : ""}`}>
+              <button
+                onClick={() => {
+                  setSortOpen(false);
+                  sortByNewest();
+                }}
+              >
+                Date - newest first
+              </button>
+              <button
+                onClick={() => {
+                  setSortOpen(false);
+                  sortByOldest();
+                }}
+              >
+                Date - oldest first
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {filteredList.length === 0 ? (
+          <h1 className="emptyMessage">No notes found</h1>
+        ) : (
+          filteredList.map((note) => (
+            <div
+              key={note._id}
+              className="note"
+              onClick={() => navigate(`/note/${note._id}`)}
+            >
+              <h2 className="noteTitle">{note.title}</h2>
+              <div className="noteMeta">
+                <h3 className="createdAt">
+                  Created :{" "}
+                  {new Date(note.createdAt).toLocaleDateString("en-IN")}
+                </h3>
+                <h3 className="lastModified">
+                  Updated :{" "}
+                  {new Date(note.updatedAt).toLocaleDateString("en-IN")}
+                </h3>
+              </div>
+              <p className="bodySnippet">{note.body}</p>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
